0278f2bb760df507f1854969e49aa612509d33d1
Miguel <miguel.rochacaetano97@gmail.com>
2019-12-17 11:23:27 +0000

    A prepara o ambientes de desemvolvimento

---
+++ Client.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Neste ficheiro é onde vai configurado variaveis como
+ * -> Download Folder
+ * -> HTML File Path
+ * -> Entre outros
+ * É aqui que vao ser chamadas todas as classe para :
+ * -> Processar o JSON e construir o url das imagens;
+ * -> Fazer o download das Imagens;
+ * -> Construir o histórico em HTML;
+ *
+ * P.S "tentar usar biblioteca de interfaces php-gtk.net"
+ *
+ *
+ */
