826e335d327b9cec6e4799f8d3306362e405a332
Daniel Carvalho <Daniel.carvalho2009@hotmail.com>
2019-12-23 14:32:24 +0000

    Recebe e processa as palavras chave

--- Client.php
+++ Client.php
@@ -14,3 +14,34 @@
  *
  *
  */
+
+define ("SLOT_DA_ULTIMA_PALAVRA", count($argv)-1);
+
+function recolherTexto(){
+    global $argc, $argv;
+    $iQuantidadeDeArgumentosRecebidos = $argc;
+    $strTextoAcrescentado = "";
+    $bHaTextoParaJuntar =  $iQuantidadeDeArgumentosRecebidos>1;
+
+    if($bHaTextoParaJuntar){
+        for($i = 1; //inicialização da variavel
+            $i <= SLOT_DA_ULTIMA_PALAVRA; //condição para percorrer a(s) string(s) de palavras recebidas
+            $i++){
+            $strPalavra = $argv[$i];
+
+            if($i === SLOT_DA_ULTIMA_PALAVRA)
+                /*
+                 * caso o numero de palavras seja 1 como i = 1
+                 * não será necessário acrescentar nada
+                 * logo a palavra ficará igual sem qualquer
+                 * acrescento
+                 */
+                $strTextoAcrescentado .= $strPalavra;
+                else
+                    //caso tenha mais que uma palavra irá adicionar um "+"
+                    $strTextoAcrescentado .= $strPalavra."+";
+        }//for
+    }//if
+    return $strTextoAcrescentado;
+}//recolherTexto
+echo (recolherTexto());
