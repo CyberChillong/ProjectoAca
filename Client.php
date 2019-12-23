85665088f9e0853d9483cd88f0134a5c7cc92fdd
Miguel <miguel.rochacaetano97@gmail.com>
2019-12-23 16:17:41 +0000

    Inico da Construção da NasaConsumer, foi criado o metodo construtor e o metodo que controi os url dos json possiveis e retorna o array con os mesmos

--- Client.php
+++ Client.php
@@ -1,16 +1,27 @@
 <?php
 
 /*
+ * Utilizar o argv para buscar a keyword //Client.php
+ * NasaConsumer(pArgumentosArgV) Miguel ainda em desenvolvimento:
+ * -> O metodo que retorna um array com todos os urls de json possiveis com conteudos unicos;
  * Neste ficheiro é onde vai configurado variaveis como
  * -> Download Folder
  * -> HTML File Path
  * -> Entre outros
- * É aqui que vao ser chamadas todas as classe para :
- * -> Processar o JSON e construir o url das imagens;
- * -> Fazer o download das Imagens;
- * -> Construir o histórico em HTML;
- *
+ * É aqui que vao ser chamadas todas as classe para:
+ * -> Processar o JSON e construir o url das  imagens (NasaConsumer);
+ * -> Guardar a pesquisa na base dados e os urls das imagens;
+ * -> Fazer o download das Imagens(AmUtil);
+ * // imagesDownload(pData);
+ * // historyBuilderOfDay(); // construia o documento html com as respetivas images;
+ * -> Construir o histórico em HTML(HtmlBuilder);
+ *Pesquisa -> Imagens
  * P.S "tentar usar biblioteca de interfaces php-gtk.net"
  *
  *
  */
+
+include("NasaConsumer.php");
+
+$b = new NasaConsumer("Mars");
+var_dump($b->builderOfViableUrls());
--- NasaConsumer.php
+++ NasaConsumer.php
@@ -1,7 +1,42 @@
 <?php
 
 class NasaConsumer {
+    public $keyword ; // key word da pesquisa
+
+    function __construct($pKeyWord) // construtor que recebe uma string que é a Keyword da pesquisa
+    {
+        $this->keyword = $pKeyWord;
+
+    }//__construct
+
+    public function builderOfViableUrls():Array{
+        /*
+         * função que vai construir todos os urls
+         * relatiovs ao json que contem as imagens*/
+        $bUrlIsvalid = true; //paremtro de continuação do ciclo
+        $itenerator = 0; // parametro que servira com itenerador que percorrera as paginas
+        $previousUrlIdentity="";//varialvel que guarda o sha1 do conteudo do json anterios
+        $AllValidUrls =[];//variavel que ira guardar todos os urls possiveis
+        while($bUrlIsvalid){
+            $url = sprintf("https://www.jpl.nasa.gov/assets/json/getMore.php?images=true&search=%s&category=&page=%o",
+            $this->keyword,
+                $itenerator
+            );// construção do url
+            $urlIdentity = sha1(file_get_contents($url));//codificação do json em sha1
+
+             if ($urlIdentity !== $previousUrlIdentity){//verivicação se o json é unico
+                 array_push($AllValidUrls, $url);//adiciona-se o url ao array
+                 $previousUrlIdentity = $urlIdentity;// guarda-se o sha1 par comparar com o proxim
+                 $itenerator++;//adiciona-se o itenerador
+             }else{
+                 $bUrlIsvalid = false;//caso o json não seja unico acaba-se o cilco
+             }
+
+        }
 
 
+        return $AllValidUrls; // retorna o array com os url conseguidos
+
+    }//builderOfViableUrls
 
 }//NasaConsumer
